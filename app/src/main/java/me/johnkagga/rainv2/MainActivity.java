3dcbb9e985102acec089e81d88be4c84c745a6a4
jokamjohn <johnkagga@gmail.com>
2015-06-16 15:29:08 +0300

    Added okhttp

---
+++ app/src/main/java/me/johnkagga/rainv2/MainActivity.java
@@ -0,0 +1,73 @@
+package me.johnkagga.rainv2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "5a20df57556bd8bcb4924c5a083bc653";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/ "
+                + latitude + "," + longitude;
+
+        /*
+        calling the client and setting up the request
+         */
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    Log.d(TAG, response.body().string());
+                    if (response.isSuccessful()) {
+                        //logging the whole response body
+
+                    } else {
+
+                    }
+                } catch (IOException e) {
+                    Log.v(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main thread is working");
+
+    }
+
+    private void alertUserAboutError() {
+
+    }
+}
+
+
+
